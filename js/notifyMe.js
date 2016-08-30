aa827e0b2d776cf9b5bf253f5b5bc11b3369fc26
Adnan Somani <adnan.somani@gmail.com>
2016-08-30 14:36:55 -0400

    new changes

--- js/notifyMe.js
+++ js/notifyMe.js
@@ -19,56 +19,59 @@
                 $(".fa-spinner").addClass("fa-spin").removeClass("opacity-0");
                 o.show();
                 e.ajax({
-                    type: "POST",
-                    url: s,
-                    data: {
+                    method: "POST",
+                      data: {
                         email: h
                     },
+                    url: "https://formspree.io/hamiltonconnectme@gmail.com",
                     dataType: "jsonp",
-                    error: function(e) {
-                        o.hide();
-                        $(".fa-spinner").addClass("opacity-0").removeClass("fa-spin");
-                        $(".block-message").addClass("show-block-error").removeClass("show-block-valid");
-                        if (e.status == 404) {
-                            $(".message").html('<p class="notify-valid">Service is not available at the moment.<br>Please check your internet connection or try again later.</p>').fadeIn();
-                        } else {
-                            $(".message").html('<p class="notify-valid">Oops. Looks like something went wrong.<br>Please try again later.</p>').fadeIn();
-                        }
-                    }
-                }).done(function(e) {
-                    o.hide();
-                    if (e.status == "success") {
-                        $(".fa-spinner").addClass("opacity-0").removeClass("fa-spin");
-                        $(".message").removeClass("bad-email").addClass("success-full");
-                        $(".block-message").addClass("show-block-valid").removeClass("show-block-error");
-                        $(".message").html('<p class="notify-valid">Congrats! You are on the list.<br>Keep an eye on your inbox for the lastest opportunities.</p>').fadeIn();
-                        i.val() = "";
-                    } else {
-                        if (e.type == "ValidationError") {
-                            $(".fa-spinner").addClass("opacity-0").removeClass("fa-spin");
-                            $(".message").html('<p class="notify-valid">This email address looks fake or invalid.<br>Please enter a real email address.</p>').fadeIn();
-                        } else {
-                            $(".fa-spinner").addClass("opacity-0").removeClass("fa-spin");
-                            $(".message").html('<p class="notify-valid">Oops. Looks like something went wrong.<br>Please try again later.</p>').fadeIn();
-                        }
-                    }
-                })
-            } else {
-                $(".fa-spinner").addClass("opacity-0").removeClass("fa-spin");
-                $(".message").addClass("bad-email").removeClass("success-full");
-                $(".block-message").addClass("show-block-error").removeClass("show-block-valid");
-                $(".message").html('<p class="notify-valid">Your e-mail address is incorrect.<br>Please check it and try again.</p>').fadeIn();
-                o.hide();
-            }
-
-            // Reset and hide all messages on .keyup()
-            $("#notifyMe input").keyup(function() {
-                $(".block-message").addClass("").removeClass("show-block-valid show-block-error");
-                $(".message").fadeOut();
-            });
-        })
-    }
+//                    error: function(e) {
+//                        o.hide();
+//                        $(".fa-spinner").addClass("opacity-0").removeClass("fa-spin");
+//                        $(".block-message").addClass("show-block-error").removeClass("show-block-valid");
+//                        if (e.status == 404) {
+//                            $(".message").html('<p class="notify-valid">Service is not available at the moment.<br>Please check your internet connection or try again later.</p>').fadeIn();
+//                        } else {
+//                            $(".message").html('<p class="notify-valid">Oops. Looks like something went wrong.<br>Please try again later.</p>').fadeIn();
+//                        }
+//                    }
+//                }).done(function(e) {
+//                    o.hide();
+//                    if (e.status == "success") {
+//                        $(".fa-spinner").addClass("opacity-0").removeClass("fa-spin");
+//                        $(".message").removeClass("bad-email").addClass("success-full");
+//                        $(".block-message").addClass("show-block-valid").removeClass("show-block-error");
+//                        $(".message").html('<p class="notify-valid">Congrats! You are on the list.<br>Keep an eye on your inbox for the lastest opportunities.</p>').fadeIn();
+//                        i.val() = "";
+//                    } else {
+//                        if (e.type == "ValidationError") {
+//                            $(".fa-spinner").addClass("opacity-0").removeClass("fa-spin");
+//                            $(".message").html('<p class="notify-valid">This email address looks fake or invalid.<br>Please enter a real email address.</p>').fadeIn();
+//                        } else {
+//                            $(".fa-spinner").addClass("opacity-0").removeClass("fa-spin");
+//                            $(".message").html('<p class="notify-valid">Oops. Looks like something went wrong.<br>Please try again later.</p>').fadeIn();
+//                        }
+//                    }
+//                })
+//            } else {
+//                $(".fa-spinner").addClass("opacity-0").removeClass("fa-spin");
+//                $(".message").addClass("bad-email").removeClass("success-full");
+//                $(".block-message").addClass("show-block-error").removeClass("show-block-valid");
+//                $(".message").html('<p class="notify-valid">Your e-mail address is incorrect.<br>Please check it and try again.</p>').fadeIn();
+//                o.hide();
+//            }
+//
+//            // Reset and hide all messages on .keyup()
+//            $("#notifyMe input").keyup(function() {
+//                $(".block-message").addClass("").removeClass("show-block-valid show-block-error");
+//                $(".message").fadeOut();
+//            });
+//        })
+    })
 
     
 
+
+            }}
+                  )}
 })(jQuery)

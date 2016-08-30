113f9eaf892a4883d24159422cc6406e2370739e
Adnan Somani <adnan.somani@gmail.com>
2016-08-30 13:37:46 -0400

    new changes

--- js/notifyMe.js
+++ js/notifyMe.js
@@ -41,7 +41,8 @@
                         $(".fa-spinner").addClass("opacity-0").removeClass("fa-spin");
                         $(".message").removeClass("bad-email").addClass("success-full");
                         $(".block-message").addClass("show-block-valid").removeClass("show-block-error");
-                        $(".message").html('<p class="notify-valid">Congrats! You are in list.<br>We will inform you as soon as we finish.</p>').fadeIn();
+                        $(".message").html('<p class="notify-valid">Congrats! You are on the list.<br>Keep an eye on your inbox for the lastest opportunities.</p>').fadeIn();
+                        i.val() = "";
                     } else {
                         if (e.type == "ValidationError") {
                             $(".fa-spinner").addClass("opacity-0").removeClass("fa-spin");
